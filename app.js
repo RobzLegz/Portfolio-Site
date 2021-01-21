b508e7a5a94c685513a484ecf3ccaef074782bec
Robzlegz <robzlegz@gmail.com>
2021-01-21 12:04:02 +0200

    small changes

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 window.addEventListener("scroll",function(){
     const activeLink = document.querySelector(".active-link");
+    const nav = document.querySelector("nav");
     activeLink.classList.add("scrolled-link");
     activeLink.classList.remove(".active-link");
+    nav.classList.add("scrolled-nav");
 })
