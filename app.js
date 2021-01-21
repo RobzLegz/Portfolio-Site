42315b4e7eb970def141b8e004e791e3155c7e2c
Robzlegz <robzlegz@gmail.com>
2021-01-21 11:44:44 +0200

    on scroll animations

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 window.addEventListener("scroll",function(){
     const activeLink = document.querySelector(".active-link");
     activeLink.classList.add("scrolled-link");
+    activeLink.classList.remove(".active-link");
 })
